dd26032abfc1494bd8d031f8e3fd69aa51bc9f25
student <Gamer_Z>
2018-09-11 03:44:14 -0700

    Do not rely on time

--- assignment1/src/cmd.cpp
+++ assignment1/src/cmd.cpp
@@ -1,72 +1,98 @@
+#include <iostream>
+
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
 #include <assignment1/Triangle.h>
+#include <assignment1/MoveRotate.h>
 
 ros::Publisher velocity_publisher;
-ros::Publisher own_publisher;
-ros::Subscriber own_subscriber;
+ros::Publisher publisher_draw_triangle;
+ros::Publisher publisher_move_rotate;
+ros::Subscriber subscriber_draw_triangle;
+ros::Subscriber subscriber_move_rotate;
 ros::Subscriber pose_subscriber;
 
 turtlesim::Pose turtlesim_pose;
 
 void PoseCallback(const turtlesim::Pose::ConstPtr& message);
 void DrawTriangleCallback(const assignment1::Triangle::ConstPtr& message);
+void MoveRotateCallback(const assignment1::MoveRotate::ConstPtr& message);
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "cmd");
 	ros::NodeHandle n;
 
-	own_publisher = n.advertise<assignment1::Triangle>("cmd/draw_triangle", 10);
+	publisher_draw_triangle = n.advertise<assignment1::Triangle>("cmd/draw_triangle", 10);
+	publisher_move_rotate = n.advertise<assignment1::MoveRotate>("cmd/move_rotate", 10);
 	velocity_publisher = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 10);
 	pose_subscriber = n.subscribe("/turtle1/pose", 10, PoseCallback);
-	own_subscriber = n.subscribe("cmd/draw_triangle", 10, DrawTriangleCallback);
+	subscriber_draw_triangle = n.subscribe("cmd/draw_triangle", 10, DrawTriangleCallback);
+	subscriber_move_rotate = n.subscribe("cmd/move_rotate", 10, MoveRotateCallback);
+
+	std::cout << "main: Prepared callbacks, spinning..." << std::endl;
 
 	ros::spin();
 
 	return 0;
 }
 
-void AbstractKeepSendingMessage(const geometry_msgs::Twist& msg, double target_value, double target_speed)
+double Sign(double a)
 {
-	double t0 = ros::Time::now().toSec();
-	double current_value = 0.0;
-	ros::Rate loop_rate(1000);
+	return a > 0.0 ? 1.0 : -1.0;
+}
 
-	while (current_value<target_value)
+double DeltaE(double a, double b)
+{
+	return (a - b) * 100.0;
+}
+
+double Distance(const turtlesim::Pose& a, const turtlesim::Pose& b)
+{
+	return sqrt(pow(a.x - b.x, 2.0) + pow(a.y - b.y, 2.0));
+}
+
+void MoveRotate(double speed, double move_distance, double rotate_radians)
+{
+	std::cout << "Received MoveRotate(" << speed << ", " << move_distance << ", " << rotate_radians << ")" << std::endl;
+
+	ros::Rate loop_rate(10);
+	geometry_msgs::Twist twist;
+
+	turtlesim::Pose saved_pose = turtlesim_pose;
+	bool loop = true;
+
+	do
 	{
-		velocity_publisher.publish(msg);
-		double t1 = ros::Time::now().toSec();
-		current_value = target_speed * (t1 - t0);
+		twist.linear.x = speed * DeltaE(Distance(saved_pose, turtlesim_pose), abs(move_distance)) * Sign(move_distance);
+		twist.angular.z = speed * DeltaE(saved_pose.theta + rotate_radians, turtlesim_pose.theta);
+
+		loop = 
+			abs(twist.linear.x) > 0.1 || 
+			abs(twist.angular.z) > 0.1;
+		if (!loop)
+		{
+			std::cout << "Aborting loop in MoveRotate(" << speed << ", " << move_distance << ", " << rotate_radians << ")" << std::endl;
+		}
+		else
+		{
+			std::cout << "(" << saved_pose.x << ", " << saved_pose.y << ") -> (" << turtlesim_pose.x << ", " << turtlesim_pose.y << "): " << (DeltaE(Distance(saved_pose, turtlesim_pose), abs(move_distance)) * Sign(move_distance)) << std::endl;
+		}
+
+		velocity_publisher.publish(twist);
 		ros::spinOnce();
 		loop_rate.sleep();
-	}
+	} while (loop);
 
 	velocity_publisher.publish(geometry_msgs::Twist());
 }
 
-void Move(double speed, double distance)
-{
-	geometry_msgs::Twist vel_msg;
-	vel_msg.linear.x = speed;
-
-	AbstractKeepSendingMessage(vel_msg, distance, speed);
-}
-
-void Rotate(double angular_speed, double relative_angle) {
-
-	geometry_msgs::Twist vel_msg;
-	vel_msg.angular.z = angular_speed;
-
-	AbstractKeepSendingMessage(vel_msg, relative_angle, angular_speed);
-}
-
-double SetAngle(double radians)
+void SetAngle(double radians)
 {
 	double delta = radians - turtlesim_pose.theta;
-	Rotate(delta, delta);
+	MoveRotate(1.0, 0.0, delta);
 }
 
 void PoseCallback(const turtlesim::Pose::ConstPtr& message)
@@ -87,12 +113,17 @@ void DrawTriangle(float side_length, bool cw)
 	SetAngle(0.0);
 	for (size_t i = 0; i < 3; ++i)
 	{
-		Move(10.0, side_length);
-		Rotate(10.0, degrees2radians(60.0));
+		MoveRotate(1.0, side_length, 0.0);
+		MoveRotate(1.0, 0.0, degrees2radians(60.0));
 	}
 }
 
 void DrawTriangleCallback(const assignment1::Triangle::ConstPtr& message)
 {
 	DrawTriangle(message->sideLength, message->clockwise);
+}
+
+void MoveRotateCallback(const assignment1::MoveRotate::ConstPtr& message)
+{
+	MoveRotate(message->speed, message->distance, message->angle);
 }
