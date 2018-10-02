ac59d04cdb14c0d2489ed60c3adc4e7205b3671c
student <Gamer_Z>
2018-10-02 02:40:09 -0700

    add base subscriber/publisher

--- assignment2/src/steering_servoing_node.cpp
+++ assignment2/src/steering_servoing_node.cpp
@@ -5,16 +5,37 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
 
-std::string name = "steering_pointshoot";
+ros::Publisher velocity;
+ros::Subscriber odom;
+ros::Subscriber goal;
+
+nav_msgs::Odometry current;
+
+void OdomCallback(const nav_msgs::OdometryConstPtr& message)
+{
+	current = *message;
+}
+
+void GoalCallback(const geometry_msgs::PointConstPtr& message)
+{
+
+}
+
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, name);
+	ros::init(argc, argv, "steering_servoing");
 	ros::NodeHandle n;
 	
+	velocity = n.advertise<geometry_msgs::Twist>("/stagesim/cmd_vel", 10);
+	odom = n.subscribe("/stagesim/odom", 10, OdomCallback);
+	goal = n.subscribe("/stagesim/goal", 10, GoalCallback);
+
 	std::cout << "main: Prepared callbacks, spinning..." << std::endl;
 
 	ros::spin();
 
 	return 0;
 }
+
