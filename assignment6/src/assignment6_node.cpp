2c57d02e2a9abba29742e6c11be2c76bec186401
student <Gamer_Z>
2018-12-11 14:19:52 -0800

    add goals

--- assignment6/src/assignment6_node.cpp
+++ assignment6/src/assignment6_node.cpp
@@ -4,46 +4,6 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-std::vector <move_base_msgs::MoveBaseGoal> createNavPoints() {
-    std::vector <move_base_msgs::MoveBaseGoal> goals;
-
-    move_base_msgs::MoveBaseGoal goal;
-    goal.target_pose.header.frame_id = "map";
-    goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x = 0.34;
-    goal.target_pose.pose.position.y = 0.07;
-    goal.target_pose.pose.orientation.x = 0;
-    goal.target_pose.pose.orientation.y = 0;
-    goal.target_pose.pose.orientation.z = -0.218410;
-    goal.target_pose.pose.orientation.w = 0.975857;
-    goals.push_back(goal);
-    goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x = 0.97;
-    goal.target_pose.pose.position.y = -0.82;
-    goal.target_pose.pose.orientation.z = -0.171565;
-    goal.target_pose.pose.orientation.w = 0.985173;
-        goals.push_back(goal);
-    goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x = 2.4;
-    goal.target_pose.pose.position.y = -1.0;
-    goal.target_pose.pose.orientation.z = 0.125737;
-    goal.target_pose.pose.orientation.w = 0.992064;
-    goals.push_back(goal);
-    goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x = 2.5;
-    goal.target_pose.pose.position.y = -0.01;
-    goal.target_pose.pose.orientation.z = 0.914425;
-    goal.target_pose.pose.orientation.w = 0.404755;
-    goals.push_back(goal);
-    goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.pose.position.x = 1.18;
-    goal.target_pose.pose.position.y = -0.11;
-    goal.target_pose.pose.orientation.z = 0.996741;
-    goal.target_pose.pose.orientation.w = -0.080668;
-    goals.push_back(goal);
-    return goals;
-}
-
 int main(int argc, char** argv) {
     ros::init(argc, argv, "simple_navigation_goals");
 
@@ -55,19 +15,51 @@ int main(int argc, char** argv) {
         ROS_INFO("Waiting for the move_base action server to come up");
     }
 
-    std::vector <move_base_msgs::MoveBaseGoal> goals = createNavPoints();
+	// isn't big so stack is fine
+	std::vector <move_base_msgs::MoveBaseGoal> goals;
 
-    ROS_INFO("Sending goals");
+	move_base_msgs::MoveBaseGoal goal;
+	goal.target_pose.header.frame_id = "map";
+	goal.target_pose.pose.position.x = 0.34;
+	goal.target_pose.pose.position.y = 0.07;
+	goal.target_pose.pose.orientation.x = 0;
+	goal.target_pose.pose.orientation.y = 0;
+	goal.target_pose.pose.orientation.z = -0.218410;
+	goal.target_pose.pose.orientation.w = 0.975857;
+	goals.push_back(goal);
+	goal.target_pose.pose.position.x = 0.97;
+	goal.target_pose.pose.position.y = -0.82;
+	goal.target_pose.pose.orientation.z = -0.171565;
+	goal.target_pose.pose.orientation.w = 0.985173;
+	goals.push_back(goal);
+	goal.target_pose.pose.position.x = 2.4;
+	goal.target_pose.pose.position.y = -1.0;
+	goal.target_pose.pose.orientation.z = 0.125737;
+	goal.target_pose.pose.orientation.w = 0.992064;
+	goals.push_back(goal);
+	goal.target_pose.pose.position.x = 2.5;
+	goal.target_pose.pose.position.y = -0.01;
+	goal.target_pose.pose.orientation.z = 0.914425;
+	goal.target_pose.pose.orientation.w = 0.404755;
+	goals.push_back(goal);
+	goal.target_pose.pose.position.x = 1.18;
+	goal.target_pose.pose.position.y = -0.11;
+	goal.target_pose.pose.orientation.z = 0.996741;
+	goal.target_pose.pose.orientation.w = -0.080668;
+	goals.push_back(goal);
 
-    for (auto g : goals) {
-        g.target_pose.header.stamp = ros::Time::now();
-        ac.sendGoal(g);
+    for (auto goal : goals) 
+	{
+		ROS_INFO("Sending goal");
+		goal.target_pose.header.stamp = ros::Time::now();
+        ac.sendGoal(goal);
         ac.waitForResult();
 
-        if(ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED) {
+        if(ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
+		{
             ROS_WARN("The base failed to move");
         }
     }
-    getchar();
+
     return 0;
 }
